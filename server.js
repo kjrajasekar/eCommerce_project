006793e9b2f0b1568f067e96099dc05c9501f862
Rajasekar <35947322+kjrajasekar@users.noreply.github.com>
2022-10-06 13:02:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const app = require('./app/app')
-const port=process.env.API_PORT;
+const port=process.env.API_PORT||4446;
 
 app.listen(port, () => {
     console.log('Server running '+port);
